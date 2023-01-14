9d9bfa04a5eeb971ba6c9f9d5eb06d091a8625df
hzgand <101759753+hzgand@users.noreply.github.com>
2023-01-14 16:11:54 -0500

    Initial Job API and search
    /api/jobs
    /api/jobs/:jobId

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const config = require('./server/config');
 
+const jobRoutes = require("./server/routes/job");
+
 require('./server/config/dbConnection');
 
 const app = express();
@@ -10,6 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
+
+app.use('/', jobRoutes);
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
