4f4f6f70f18951e32fdfad8c8995601364473cd9
hzgand <101759753+hzgand@users.noreply.github.com>
2023-01-14 11:31:05 -0500

    dotenv mongodb connection

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config();
+
+const mongoUSER = process.env.MONGODB_USER;
+const mongoPASS = process.env.MONGODB_PASS;
+
 const config = {
 	port: process.env.PORT || 4000,
 	jwtSecret: process.env.JWT_SECRET || 'jwtSecret',
-	mongoURI: process.env.MONGODB_URI || 'mongodb://localhost/job-app'
+	mongoURI: process.env.MONGODB_URI || `mongodb+srv://${mongoUSER}:${mongoPASS}@cluster0.ha0me1n.mongodb.net/test`
 };
 
 module.exports = config;
