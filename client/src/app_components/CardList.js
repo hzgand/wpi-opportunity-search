c3f2cc6ca28c76c2ea3180e6b50a48517a3d55da
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-01-15 09:43:05 -0500

    Updates
    Updates

--- client/src/app_components/CardList.js
+++ client/src/app_components/CardList.js
@@ -8,41 +8,49 @@ import LinearProgress from "@mui/material/LinearProgress";
 class CardList extends React.Component {
   constructor() {
     super();
-    this.state = { jobs: [], doneLoading: false};
+    this.state = { jobs: [], doneLoading: false };
   }
 
   componentDidMount() {
     fetch("http://localhost:3000/api/jobs")
       .then((response) => response.json())
       .then((data) => {
-        this.setState({ jobs: data, doneLoading: true});
+        this.setState({ jobs: data, doneLoading: true });
       });
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.searchString !== this.props.searchString || nextState !== this.state;
+    return (
+      nextProps.searchString !== this.props.searchString ||
+      nextState !== this.state
+    );
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.searchString !== this.props.searchString) {
+      this.setState({ jobs: [], doneLoading: false });
       fetch(`http://localhost:3000/api/jobs?q=${this.props.searchString}`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ jobs: data, doneLoading: true});
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({ jobs: data, doneLoading: true });
+        });
     }
   }
 
   render() {
     return (
       <Box>
-        {this.state.doneLoading ? this.state.jobs.map((jobItem) => {
-          return (
-            <ListItem key={jobItem._id}>
-              <CardHeaderExample job={jobItem} />
-            </ListItem>
-          );
-        }) : <LinearProgress/>}
+        {this.state.doneLoading ? (
+          this.state.jobs.map((jobItem) => {
+            return (
+              <ListItem key={jobItem._id}>
+                <CardHeaderExample job={jobItem} />
+              </ListItem>
+            );
+          })
+        ) : (
+          <LinearProgress />
+        )}
       </Box>
     );
   }
