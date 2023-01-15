bc8ee78ad4c421d9427cac45b6b92fc1022231e7
Mahir Sowad <32722691+mahirsowad3@users.noreply.github.com>
2023-01-14 22:03:50 -0500

    Icons
    Icons

--- client/src/app_components/CardHeaderExample.js
+++ client/src/app_components/CardHeaderExample.js
@@ -11,90 +11,143 @@ import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import EmailIcon from "@mui/icons-material/Email";
 import ShareIcon from "@mui/icons-material/Share";
 import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
+import QueryBuilderIcon from "@mui/icons-material/QueryBuilder";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+
+const config = {
+  iconPadding: "0.5rem",
+};
 
 export default function ImgMediaCard(props) {
   return (
-    <Card sx={{ width: 0.65, margin: "auto" }}>
-      <Box sx={{ display: "flex", flexDirection: "row" }}>
-        <CardHeader
-          avatar={
-            <Avatar
-              sx={{ fontSize: "2rem", width: 100, height: 100 }}
-              aria-label="recipe"
-            >
-              AI
-            </Avatar>
-          }
-        />
+    <Card sx={{ width: 0.6, margin: "auto" }}>
+      <CardActionArea>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <CardHeader
+            avatar={
+              <Avatar
+                sx={{ fontSize: "2rem", width: 100, height: 100 }}
+                aria-label="recipe"
+              >
+                AI
+              </Avatar>
+            }
+          />
 
-        <CardContent sx={{ width: 1 }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyItems: "space-evenly",
-              width: 1,
-            }}
-          >
-            <Box>
-              <Typography gutterBottom variant="h5" component="h2">
-                {props.job.positionTitle}
-              </Typography>
-            </Box>
+          <CardContent sx={{ width: 1 }}>
             <Box
               sx={{
                 display: "flex",
-                flexDirection: "row",
+                flexDirection: "column",
+                justifyItems: "space-evenly",
+                width: 1,
               }}
             >
+              <Box>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {props.job.positionTitle}
+                </Typography>
+              </Box>
               <Box
                 sx={{
                   display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-evenly",
-                  paddingRight: "10rem",
+                  flexDirection: "row",
                 }}
               >
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {props.job.department}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {props.job.location}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {props.job.hoursPerWeek}{" "}
-                  {props.job.hoursPerWeek === 1 ? "hr/week" : "hrs/week"}
-                </Typography>
-              </Box>
-              <Box>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  <Box sx={{ display: "flex", flexDirection: "row" }}>
-                    <AssuredWorkloadIcon />
-                    {props.job.federalFunding
-                      ? "Work Study Required"
-                      : "Work Study Not Required"}
-                  </Box>
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {" "}
-                  <Box sx={{ display: "flex", flexDirection: "row" }}>
-                    <CalendarMonthIcon
-                      sx={{ paddingRight: "0.1rem" }}
-                    ></CalendarMonthIcon>
-                    {props.job.jobType}
-                  </Box>
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  <Box sx={{ display: "flex", flexDirection: "row" }}>
-                    <EmailIcon sx={{ paddingRight: "0.1rem" }}></EmailIcon>
-                    {props.job.email}
-                  </Box>
-                </Typography>
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-evenly",
+                    paddingRight: "10rem",
+                  }}
+                >
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <ApartmentIcon
+                        sx={{ paddingRight: config.iconPadding }}
+                      ></ApartmentIcon>
+                      {props.job.department}
+                    </Box>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <LocationOnIcon
+                        sx={{ paddingRight: config.iconPadding }}
+                      ></LocationOnIcon>
+                      {props.job.location}
+                    </Box>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <EmailIcon
+                        sx={{ paddingRight: config.iconPadding }}
+                      ></EmailIcon>
+                      {props.job.email}
+                    </Box>
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <AssuredWorkloadIcon
+                        sx={{ paddingRight: config.iconPadding }}
+                      />
+                      {props.job.federalFunding
+                        ? "Work Study Required"
+                        : "Work Study Not Required"}
+                    </Box>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    {" "}
+                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <CalendarMonthIcon
+                        sx={{ paddingRight: config.iconPadding }}
+                      ></CalendarMonthIcon>
+                      {props.job.jobType}
+                    </Box>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                  >
+                    <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <QueryBuilderIcon
+                        sx={{ paddingRight: config.iconPadding }}
+                      />
+                      {props.job.hoursPerWeek}{" "}
+                      {props.job.hoursPerWeek === 1 ? "hr/week" : "hrs/week"}
+                    </Box>
+                  </Typography>
+                </Box>
               </Box>
             </Box>
-          </Box>
-        </CardContent>
-      </Box>
+          </CardContent>
+        </Box>
+      </CardActionArea>
     </Card>
   );
 }
