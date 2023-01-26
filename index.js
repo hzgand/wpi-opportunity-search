8d0880df3e1cffa0136f825c5716b130147d379a
hzgand <101759753+hzgand@users.noreply.github.com>
2023-01-25 20:09:19 -0500

    Static serve index.js
    build within client directory, then npm run server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const config = require('./server/config');
+const path = require('path');
 
 const jobRoutes = require("./server/routes/job");
 
@@ -21,6 +22,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
 app.use('/', jobRoutes);
+
+console.log(path.join(__dirname, 'frontend', 'build'));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
