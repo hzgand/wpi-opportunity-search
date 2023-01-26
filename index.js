179300d97c7540211e8b101edf4350741adfe7cd
hzgand <101759753+hzgand@users.noreply.github.com>
2023-01-26 18:00:32 -0500

    node_env variable

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ app.use(cookieParser());
 
 app.use('/', jobRoutes);
 
-console.log(path.join(__dirname, 'frontend', 'build'));
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+if(process.env.NODE_ENV === "production") {
+    console.log(path.join(__dirname, 'frontend', 'build'));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-app.get("*", async (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+    app.get("*", async (req, res) => {
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+}
 
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
